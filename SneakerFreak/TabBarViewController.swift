fbc34639d0657f49462c9b0554eece59beedf1bd
Emirock11 <emi.rock11@hotmail.com>
2020-04-22 18:40:24 -0500

    Se han agregado los documentos a la carpeta del repositorio

---
+++ SneakerFreak/TabBarViewController.swift
@@ -0,0 +1,31 @@
+//
+//  TabBarViewController.swift
+//  Mapa01
+//
+//  Created by Emiliano Antonio Pineda Hernandez on 22/04/20.
+//  Copyright © 2020 Tec de Monterrey. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
